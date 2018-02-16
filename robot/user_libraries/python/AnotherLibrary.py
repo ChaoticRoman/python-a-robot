7f20f38bb2a6553fe53313aa9473d964d3e1f21d
Roman Pavelka <rpavelka@redhat.com>
2018-02-16 14:34:12 +0100

    Add Python wrapping

--- robot/user_libraries/python/AnotherLibrary.py
+++ robot/user_libraries/python/AnotherLibrary.py
@@ -1,12 +1,19 @@
-class Thriller:
+from robot.api.deco import keyword
+
+class AnotherLibrary:
     """Thrilling class"""
 
-    def __init__(self):
-        pass
+    ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
+    def __init__(self):
+        self.i = 1
+
+    @keyword('Call some function')
     def some_function(self):
         """Thrilling function
 
         Does it works? Let's see...
         """
-        return 'It works!!'
+        result = self.i * 'It works!! '
+        self.i += 1
+        return result
--- robot/user_libraries/python/test.robot
+++ robot/user_libraries/python/test.robot
@@ -1,7 +1,15 @@
 *** Settings ***
-Library    ./MyLibrary.py  32
+Library    MyLibrary       32
+Library    AnotherLibrary
 
 *** Test Cases ***
 Test of Python library
     ${out} =  get  10
+    Log  ${out}
+
+Test 1
+    Call some function
+
+Test 2
+    ${out} =  Call some function
     Log  ${out}
